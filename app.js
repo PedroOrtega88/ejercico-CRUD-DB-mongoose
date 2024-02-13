91c2ce22655acc2ed63eab804b0f5ec16110fa33
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-02-13 19:50:15 +0100

    -a

--- app.js
+++ app.js
@@ -11,35 +11,51 @@ app.use('/', routes);
 dbConnection();
 
 
-// POST /
-app.post('/create', (req, res) => {
+app.post('/create', async (req, res) =>{
+  try {
+    const {titulo} = req.body;
+    const tarea = new tarea({titulo});
+    await tarea.save();
+    res.json(tarea);
+  }catch (error){
+
+    res.status(4001).json({ error:'no es posible crear esta tarea'})
+  }
+
+  });
+
+  app.get('/', async (req, res) => {
+    try {
+      const {titulo} = await tarea.find();
+    res.json(tarea);
+    }catch (error){
+  
+      res.status(4001).json({ error: 'no es posible obtener las tareas'})
+    }
+  
+    });
+
+
+
+  app.put('/markAsCompleted', async (req, res) => {
+    try {
+      const {tarea} = req.body();
+
+      if(!tarea){
+       return res.status(4001).json({ error: 'no es posible obtener las tareas'})
+    }
+    res.json(tarea)
+  }catch (error){
     
-  });
+res.status(4001).json({ error: 'no es posible obtener las tareas'})
+
+  }
   
-  // GET /
-  app.get('/', (req, res) => {
-   
-  });
-  
-  // GET /id/:_id
-  app.get('/id/:_id', (req, res) => {
+    });
+
+
+
     
-  });
-  
-  // PUT /markAsCompleted/:_id
-  app.put('/markAsCompleted/:_id', (req, res) => {
-    
-  });
-  
-  // PUT /id/:_id
-  app.put('/id/:_id', (req, res) => {
-    
-  });
-  
-  // DELETE /id/:_id
-  app.delete('/id/:_id', (req, res) => {
-  
-  });
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
