2e6db35dac4c317e7a389c1b7298355f743bc099
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-02-19 23:35:12 +0100

    -a

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('Conexión exitosa a la base de datos');
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:', error);
-    }
-};
+const dbConnection = async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Base de datos conectada con éxito')
+  } catch (error) {
+    console.log(error)
+  }
+} 
 
-module.exports = dbConnection;
+module.exports = dbConnection
--- models/task.js
+++ models/task.js
@@ -8,4 +8,3 @@ const TaskSchema = new mongoose.Schema({
 const Task = mongoose.model('Task', TaskSchema);
 
 module.exports = Task;
-
